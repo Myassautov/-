2f16151065df1d0e342a982325a031ca224a4a5a
Myassautov <136903945+Myassautov@users.noreply.github.com>
2025-02-12 16:24:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let ElDiv = document.querySelector('.header__modal')
+
+let ElbtnOpen = document.querySelector('.header__media__diplay__menu')
+
+let ElbtnClouse = document.querySelector('.header__modal__menu')
+
+
+
+
+
+let elDivModal = document.querySelector('.modal')
+
+let elDivModal2 = document.querySelector('.modal-search')
+
+function modalFunctionOpen(){
+  elDivModal.classList.add('modal-clouse')
+}
+
+function modalFunctionClouse(){
+  elDivModal.classList.remove('modal-clouse')
+}
+
+
+
+function modalFunctionOpen2(){
+  elDivModal2.classList.add('modal-search-clouse')
+}
+
+function modalFunctionClouse2(){
+  elDivModal2.classList.remove('modal-search-clouse')
+}
+    
